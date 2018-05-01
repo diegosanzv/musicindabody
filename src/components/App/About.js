c0c3b9d32888590a262e4c731c8f4bfe33c57de4
Sergi Massaneda <sergi.massaneda@gmail.com>
2018-05-01 20:12:38 +0200

    about page content

--- src/components/App/About.js
+++ src/components/App/About.js
@@ -5,8 +5,13 @@ const About = () => (
 	<div className="container">
 		<div className="row">
 			<div className="col-md-12">
-				<h5 className="pt-2 mb-3">About</h5>
-				<p>Did you get here via Redux?</p>
+				<h4 className="mt-3 mb-5">About</h4>
+				<p>Music search via <a href="https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/">iTunes search API</a></p>
+				<p>Result preview listening using the HTML5 <span className="font-monospace">&lt;audio&gt;</span> element</p>
+				<p>Build using React Redux and Bootstrap 4</p>
+				<p>Code at: <a href="https://github.com/semd/musicindabody">https://github.com/semd/musicindabody</a></p>
+				<p><strong>Author:</strong></p>
+				<p>Sergi Massaneda (<a href="https://github.com/semd/">https://github.com/semd</a>)</p>
 			</div>
 		</div>
 	</div>
